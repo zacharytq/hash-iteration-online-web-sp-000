17ce7407a9723cc1c550f2b2f6de10f11ed9c8d9
chic-page-6502 <zacharytq@gmail.com>
2019-07-16 02:19:48 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,11 +1,20 @@
 # birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
+#   "Timmy" => 9,
+#   "Sarah" => 6,
 #   "Amanda" => 27
 # }
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
+  birthday_kids.each do |kid, age|
+    puts "Happy Birthday #{kid}! You are now #{age} years old!"
+  end
 end
 
-
+def age_appropriate_birthday(birthday_kids)
+  birthday_kids.each do |kid, age|
+    if age < 13
+      puts "Happy Birthday #{kid}! You are now #{age} years old!"
+    end
+  end
+end
--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,11 +1,18 @@
 # passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
+# suite_a: "Amanda Presley",
+# suite_b: "Seymour Hoffman",
+# suite_c: "Alfred Tennyson",
+# suite_d: "Charlie Chaplin",
 # suite_e: "Crumpet the Elf"
 # }
 
 def select_winner(passengers)
   # add the code snippet here!
+  winner = ""
+  passengers.each do |key, value|
+    if key == :suite_a && value.start_with?("A")
+      winner = value
+    end
+  end
+  winner
 end

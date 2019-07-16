1e0fd7fc1817e2b3ea2794b3ec34457b23d1dbce
chic-page-6502 <zacharytq@gmail.com>
2019-07-16 02:21:23 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -15,6 +15,8 @@ def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kid, age|
     if age < 13
       puts "Happy Birthday #{kid}! You are now #{age} years old!"
+    else
+      puts "You are too old for this."
     end
   end
 end
